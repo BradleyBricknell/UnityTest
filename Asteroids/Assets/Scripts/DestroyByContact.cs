eff48f1ad2493bbf382d772fbd98e346cc3250bb
Bradley Hall <Bradley Hall>
2017-04-30 22:24:46 +0100

    Test commit

---
+++ Asteroids/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+
+    public GameObject playerExplosion;
+
+    public int scoreValue;
+
+    private GameController gameController;
+
+    void Start()
+    {
+        var gameControllerObject = GameObject.FindWithTag("GameController");
+
+        gameController = gameControllerObject.GetComponent<GameController>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {       
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+
+        Instantiate(explosion, transform.position, transform.rotation);
+
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+
+            gameController.GameOver();
+        }
+
+        gameController.AddScore(scoreValue);
+
+        Destroy(other.gameObject);
+
+        Destroy(gameObject);
+    }
+}
---
+++ Asteroids/Assets/Scripts/Pickup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public AudioClip audioClip;    
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            AudioSource.PlayClipAtPoint(audioClip, transform.position);
+
+            Destroy(gameObject);
+        }
+    }
+}
